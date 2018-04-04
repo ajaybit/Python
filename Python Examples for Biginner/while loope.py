248d40fd5deb55629e17bb33a9ff5ef32f01f3c3
PythonBytes <ajaykumar.bit.1995@gmail.com>
2018-04-04 22:43:19 +0530

    While loop!

---
+++ Python Examples for Biginner/while loope.py
@@ -0,0 +1,16 @@
+i = 0
+number = []
+
+while i < 6:
+    print "At the top i is %d" % i
+    number.append(i)
+
+    i = i+1
+    print "Number now: ", number
+
+    print "At the bottom i id %d" % i
+
+print "The number:"
+
+for num in number:
+    print num
